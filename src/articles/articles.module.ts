cea83a11a3c10185ebfc5134162451684a0a9334
Lucas Vieira <lucasltv@gmail.com>
2023-10-23 14:34:55 -0300

    wip

---
+++ src/articles/articles.module.ts
@@ -0,0 +1,13 @@
+// src/articles/articles.module.ts
+
+import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ArticlesController } from './articles.controller';
+import { ArticlesService } from './articles.service';
+
+@Module({
+  controllers: [ArticlesController],
+  providers: [ArticlesService],
+  imports: [PrismaModule],
+})
+export class ArticlesModule {}
